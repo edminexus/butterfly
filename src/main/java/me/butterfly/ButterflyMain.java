a1847a30a1097f03300aa9af88e9ee1545d8a490
edminexus <siddharthadas7974@gmail.com>
2026-01-22 10:47:37 +0530

    Renamed ButterflyPlugin to ButterflyMain

--- src/main/java/me/butterfly/ButterflyMain.java
+++ src/main/java/me/butterfly/ButterflyMain.java
@@ -1,22 +1,22 @@
 package me.butterfly;
 
 import org.bukkit.plugin.java.JavaPlugin;
-
+import me.butterfly.SaveData;
 import java.util.*;
 
-public class ButterflyPlugin extends JavaPlugin {
+public class ButterflyMain extends JavaPlugin {
 
     public final Set<UUID> enabled = new HashSet<>();
     public final Set<UUID> interacted = new HashSet<>();
     public final Map<UUID, Long> cooldowns = new HashMap<>();
 
-    public LifespanStore lifespan;
+    public SaveData lifespan;
 
     public static final long COOLDOWN_MS = 1000;
 
     @Override
     public void onEnable() {
-        lifespan = new LifespanStore(this);
+        lifespan = new SaveData(this);
         lifespan.load();
 
         CommandHandler cmd = new CommandHandler(this);
